0288888ae1e5aab780b772341f106dc018b18782
BenEight8 <contact.beneight@gmail.com>
2026-02-09 16:35:34 +0100

    Add avatar function

--- www/src/Controller/ProfilController.php
+++ www/src/Controller/ProfilController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Form\ProfilType;
+use App\Service\FileUploader;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\EntityManagerInterface; 
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\HttpFoundation\Request; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/profil')]
@@ -23,7 +24,7 @@ final class ProfilController extends AbstractController
     }
 
     #[Route('/edit', name: 'app_profil_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
     {
         /** @var \App\Entity\User $user */
         $user = $this->getUser(); // On récupère l'utilisateur connecté
@@ -32,6 +33,20 @@ final class ProfilController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Gestion de l'upload du fichier avatar
+            $avatarFile = $form->get('avatar')->getData();
+
+            if ($avatarFile) {
+                // Supprimer l'ancien avatar s'il existe
+                if ($user->getAvatar()) {
+                    $fileUploader->delete($user->getAvatar());
+                }
+
+                // Uploader le nouveau fichier dans le dossier "avatars"
+                $avatarPath = $fileUploader->upload($avatarFile, 'avatars');
+                $user->setAvatar($avatarPath);
+            }
+
             $entityManager->flush();
 
             $this->addFlash('success', 'Votre profil a été mis à jour.');
