4e17edcbace8b7d8c7f546a6695d03f4a6f1a48e
ES_PREPA_DEV <sosoros66@icloud.com>
2026-02-09 09:13:37 +0100

    add game to create team

--- www/src/Form/TeamType.php
+++ www/src/Form/TeamType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Game;
 use App\Entity\Team;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -16,8 +18,23 @@ class TeamType extends AbstractType
         $builder
             ->add('name', TextType::class, [
                 'label' => 'Nom de l\'équipe',
-                'attr' => ['placeholder' => 'Ex: Vitality, Karmine Corp, etc.']
+                'attr' => ['placeholder' => 'Ex: Vitality, Karmine Corp...']
             ])
+            
+            ->add('game', EntityType::class, [
+                'class' => Game::class,
+                'choice_label' => 'label',  
+                'label' => 'Jeu principal',
+                'placeholder' => 'Sélectionnez votre jeu de prédilection',
+                'expanded' => false,
+                'multiple' => false,
+                'required' => true, 
+                'query_builder' => function (\Doctrine\ORM\EntityRepository $er) {
+                    return $er->createQueryBuilder('g')
+                        ->orderBy('g.label', 'ASC');
+                },
+            ])
+            
             ->add('description', TextareaType::class, [
                 'label' => 'Présentation',
                 'required' => false,
